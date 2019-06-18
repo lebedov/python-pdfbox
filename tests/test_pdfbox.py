73dafeac1a4218255eb8a953b7fbabe9eeca046a
Fakabbir Amin <fakabbir@gmail.com>
2019-06-18 10:57:55 +0530

    Adds platform dependent use case

--- tests/test_pdfbox.py
+++ tests/test_pdfbox.py
@@ -4,13 +4,17 @@ from unittest import main, TestCase
 
 import pdfbox
 import os
+from sys import platform
 # To generate test PDF, process test.md with pandoc using the command
 # pandoc -t latex test.md -o test.pdf
 class test_pdfbox(TestCase):
     def test_extract(self):
         p = pdfbox.PDFBox()
         text = p.extract_text('tests/test.pdf')
-        self.assertEqual(text, 'this is a test PDF\r\n')
+        if platform == "linux" or platform == "linux2" or platform =="darwin":
+            self.assertEqual(text, 'this is a test PDF\n')
+        elif platform == "win32":
+            self.assertEqual(text, 'this is a test PDF\r\n')
 
     def test_image_extract(self):
         p = pdfbox.PDFBox()
